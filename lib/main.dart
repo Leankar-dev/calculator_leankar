1c0d478f0eead0a4e77ff5df7b8f63b6f9b0d295
Leandro <l.silva01@uol.com.br>
2026-05-11 22:37:11 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:calculator_05122025/app_calculator.dart';
+﻿import 'package:calculator_05122025/app_calculator.dart';
 import 'package:calculator_05122025/controllers/settings_controller.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
