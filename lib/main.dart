22cad3b0d505c0462da067254192cc6df29f6276
Leandro <l.silva01@uol.com.br>
2026-08-13 20:01:20 +0100

    feat(splash): integrar FlutterNativeSplash.preserve/remove no main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@
 import 'package:calculator_05122025/controllers/ad_consent_controller.dart';
 import 'package:calculator_05122025/controllers/settings_controller.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await SettingsController.instance.loadSettings();
   await AdConsentController.instance.loadPersistedConsent();
+
   runApp(const MyApp());
+
+  FlutterNativeSplash.remove();
 }
